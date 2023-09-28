7bda5d6736cc154333372fefb801b6628c19cbad
pedrodmm63 <pedrodmm63@educastur.es>
2023-09-28 14:17:49 +0200

    ejercicio 1

--- ejercicio1/ej1.js
+++ ejercicio1/ej1.js
@@ -1,22 +1,34 @@
-let c = []
+let numero = prompt("Dame un numero")
 let resultado = false;
 
-/*function esEntero(c) {
-    let numero = prompt("Dame un numero")
-
+function esEntero() {
+   
     if(parseInt(numero) == numero) resultado = true;
-    else console.error("El numero es negativo")
+    else console.error("El numero no es entero")
+    
 }
 
 esEntero();
 alert(resultado);
-*/
-function esPositivo() {
+
+function esPositivo(num) {
     let numero = prompt("Dame un numero")
-    while(numero > 0 ){ 
+    if(numero > 0 ){ 
         resultado = true;
     }
+    else console.error("El numero es negativo")
 }
 
 esPositivo();
 alert(resultado);
+
+function sumatorio() {
+
+    let resultado = 0;
+    for(var i = 0; i < numero; i++ ) {
+         resultado += i;
+    }
+    return resultado;
+}
+sumatorio();
+console.log(sumatorio());
