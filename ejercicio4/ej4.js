993a250e94318d7dc60d9940ca5acc85b5b800a7
pedrodmm63 <pedrodmm63@educastur.es>
2023-10-02 12:51:19 +0200

    Ejercicio 4

---
+++ ejercicio4/ej4.js
@@ -0,0 +1,43 @@
+var num1 = prompt("Escriba el primer numero");
+var num2 = prompt("Escriba el segundo número");
+var operador = prompt("Escriba que operación quiere realizar");
+var resultado;
+
+while(operador != "FIN") {
+switch(operador) {
+    case "+":
+        resultado = num1 + num2;
+        break;
+    case "-":
+        resultado = num1 - num2;
+        break;
+    case "*":
+        resultado = num1*num2;
+        break;
+    case "/":
+        resultado = num1/num2
+        while(isNaN(num2)) {
+            num2 = prompt("Escriba el segundo número de nuevo");
+        };   
+    }
+
+}
+
+function validaOperador() {
+    let valido = false;
+    if(operador = "+" || "-" || "*" || "/") {
+        valido = true;
+    }
+    return valido;
+}
+function divisionValida() {
+    let valido = false;
+    if(resultado == 0) {
+        valido = true;
+    }
+    return valido;
+}
+
+alert(resultado);
+console.log(validaOperador());
+console.log(divisionValida());
